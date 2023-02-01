49ef738c3c1e173e817e57e79427b261519c798a
Mark <mark@remarkablemark.org>
2023-01-31 19:02:32 -0500

    refactor: tidy index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>
